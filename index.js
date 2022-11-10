527e777f83a723960a8ce4367f0e18d8ffe65895
Saturn Dev <113562534+SaturnDevOFC@users.noreply.github.com>
2022-11-09 22:11:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
 const SimplDB = require('simpl.db');
 client.db = new SimplDB();
 
-client.login('MTAzNTMwNzc0MTQwMjY0NDUxMA.GSec2J.tZ76vs2xFXEjP8vvPlzEXowM5nDSXDx55O1sHY')
+client.login('')
 
 client.on("ready", async() => {
     console.log(`Conectei em: ${client.user.tag}`)
@@ -20,19 +20,19 @@ client.on("messageCreate", async (message) => {
     if (!message.guild || message.author.bot) return
 
     if(message.content === '!antilink off') {
-        if(message.member.permissions.has('ADMINISTRATOR')) {
-            message.reply({ content: `✅ **|** Olá ${message.member}, você acabou de desativar o antilink!` })
+        if(!message.member.permissions.has(PermissionFlagsBits.Administrator)) {
+            message.reply({ content: `<:off:1039688376066002984> **|** Olá ${message.member}, você acabou de desativar o antilink!` })
             client.db.set(`antilink.${message.guild.id}`, false)
         } else {
-            message.reply({ content: `⚠ **|** Olá ${message.member}, Você não possue permissões para usar este comando!` })
+            message.reply({ content: `<:warn:1039688369585803344> **|** Olá ${message.member}, Você não possue permissões para usar este comando!` })
         }
     }
     if(message.content === '!antilink on') {
-        if(message.member.permissions.has('ADMINISTRATOR')) {
-            message.reply({ content: `✅ **|** Olá ${message.member}, você acabou de ativar o antilink!` })
+        if(!message.member.permissions.has(PermissionFlagsBits.Administrator)) {
+            message.reply({ content: `<:on:1039688377848565781> **|** Olá ${message.member}, você acabou de ativar o antilink!` })
             client.db.set(`antilink.${message.guild.id}`, true)
         } else {
-            message.reply({ content: `⚠ **|** Olá ${message.member}, Você não possue permissões para usar este comando!` })
+            message.reply({ content: `<:warn:1039688369585803344> **|** Olá ${message.member}, Você não possue permissões para usar este comando!` })
         }
     }
 })
@@ -40,12 +40,12 @@ client.on("messageCreate", async (message) => {
     let link = /(((discord.gg?)))/;
 
     if (!message.guild) return
-	if(!int.member.permissions.has(PermissionFlagsBits.Administrator)) return
+	if(!message.member.permissions.has(PermissionFlagsBits.Administrator)) return
 
 	if(client.db.get(`antilink.${message.guild.id}`) === true ) {
 		if(link.test(message) === true) {
 		   	await message.delete().catch(saturndev => {})
-        	message.channel.send(`❌ **|** Olá ${message.member}, você não pode enviar convites de outros servidores aqui!`)
+        	message.channel.send(`<:block:1039688364103827496> **|** Olá ${message.member}, você não pode enviar convites de outros servidores aqui!`)
             .then(x => {
                 setTimeout(() => { x.delete() }, 10000)
             })
